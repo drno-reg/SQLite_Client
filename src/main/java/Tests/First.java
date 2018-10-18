9dcbd057d6d7392b9d79d29a7f0935a7f6b676d8
drno <drno-reg@mail.ru>
2018-10-18 18:20:39 +0300

    Initial commit

---
+++ src/main/java/Tests/First.java
@@ -0,0 +1,13 @@
+package Tests;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World New!!!");
+
+        System.out.println("Fuckup");
+
+    }
+
+
+}
